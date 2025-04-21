f3cfde9aacd62a941dd9cdc45f229dcb3293ae4c
jlee7362 <jlee736222@gmail.com>
2025-04-21 22:02:24 +0900

    "this참조 변수와 this()메소드"

--- src/main/java/koreaIT/Car.java
+++ src/main/java/koreaIT/Car.java
@@ -2,4 +2,15 @@ package koreaIT;
 
 public class Car {
     private String modelName;
+    private String color;
+    private int maxSpeed;
+    private int currentSpeed;
+
+    public Car(String modelName, String color, int maxSpeed, int currentSpeed) {
+        this.modelName = modelName;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.currentSpeed = currentSpeed;
+    }
 }
+
